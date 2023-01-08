76acba6a08686478aa56d6a9d2b317e9d5c13824
ljelliot <lukejelliot25@gmail.com>
2023-01-08 08:09:09 +0000

    [rash] Remove hidden from command.

--- src/shell.rs
+++ src/shell.rs
@@ -10,7 +10,6 @@ use crate::{
 type Out = (i32, String);
 
 #[cfg(unix)]
-#[doc(hidden)]
 pub fn command<S: AsRef<str>>(c: S) -> Result<Out, RashError> {
     run_command(c, &LibCWrapperImpl::new())
 }
